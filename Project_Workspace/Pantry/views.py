30be34bb605def02ee35e593c62feebfc7abafb9
MDetzel <steelersrgr8@gmail.com>
2023-04-11 22:02:40 -0500

    Added Serializer, Menu Model, and Item View

--- Project_Workspace/Pantry/views.py
+++ Project_Workspace/Pantry/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework import generics
+from .models import *
+from Pantry.serializers import *
 
 # Create your views here.
 def MainView(request):
     return HttpResponse("Congratulations! You did it!")
+
+class ItemView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
